43a195117e194415d5af9baa4a864517e5d4c997
Marco Vivi <marco.vivi@gmail.com>
2019-05-27 14:34:50 +0200

    fixed some code style errors errors

--- admin/partials/wp-mailchimp-gw-admin-display.php
+++ admin/partials/wp-mailchimp-gw-admin-display.php
@@ -16,7 +16,7 @@
 
 <div class="wrap">
 
-    <h2 class="wp-heading-inline"><?php echo esc_html(get_admin_page_title()); ?></h2>
+    <h2 class="wp-heading-inline"><?php esc_html(get_admin_page_title()); ?></h2>
 
     <form method="post" id="wp_mailchimp_gw_options" name="wp_mailchimp_gw_options" action="options.php">
 
@@ -26,22 +26,24 @@
         $options = get_option($this->plugin_name);
         ?>
 
-        <h2 class="wp-heading-inline"><?php _e('Mailchimp API Key', $this->plugin_name);?></h2>
-        <fieldset class="<?php echo $this->plugin_name; ?>-admin-api">
-            <input type="text" class="<?php echo $this->plugin_name; ?>-api" id="<?php echo $this->plugin_name; ?>-api"
-                name="<?php echo $this->plugin_name; ?>[wp_mailchimp_gw_api]"
-                value="<?php echo $options['wp_mailchimp_gw_api']; ?>" size="40" />
+        <h2 class="wp-heading-inline"><?php esc_html_e('Mailchimp API Key', $this->plugin_name);?></h2>
+        <fieldset class="<?php esc_html($this->plugin_name); ?>-admin-api">
+            <input type="text" class="<?php esc_html($this->plugin_name); ?>-api"
+                id="<?php esc_html($this->plugin_name); ?>-api"
+                name="<?php esc_html($this->plugin_name); ?>[wp_mailchimp_gw_api]"
+                value="<?php esc_html($options['wp_mailchimp_gw_api']); ?>" size="40" />
         </fieldset>
 
-        <h2 class="wp-heading-inline"><?php _e('API Endpoints', $this->plugin_name);?></h2>
+        <h2 class="wp-heading-inline"><?php esc_html_e('API Endpoints', $this->plugin_name);?></h2>
         <div class="endpoint">
             <input id="endpoint-slug" type="text" value=""
-                placeholder="<?php _e('endpoint name', $this->plugin_name);?>" />
-            <input id="endpoint-listid" type="text" value="" placeholder="<?php _e('list ID', $this->plugin_name);?>" />
+                placeholder="<?php esc_html_e('endpoint name', $this->plugin_name);?>" />
+            <input id="endpoint-listid" type="text" value=""
+                placeholder="<?php esc_html_e('list ID', $this->plugin_name);?>" />
         </div>
         <p class="submit">
             <button id="add" class="button button-primary">
-                <?php _e('Add API Endpoint', $this->plugin_name); ?>
+                <?php esc_html_e('Add API Endpoint', $this->plugin_name); ?>
             </button>
         </p>
 
@@ -49,10 +51,10 @@
             <?php
             if ( ! empty($options['endpoints']) ) {
                 foreach ( $options['endpoints'] as $key => $endpoint ) {
-                    echo '<div class="endpoint">';
-                    echo '<input type="text" name="wp-mailchimp-gw[endpoints][' . $key . '][slug]" value="' . $endpoint['slug'] . '" readonly/>';
-                    echo '<input type="hidden" name="wp-mailchimp-gw[endpoints][' . $key . '][listid]" value="' . $endpoint['listid'] . '" readonly/>';
-                    echo '<span id="remove" class="dashicons dashicons-trash"></span></div>';
+                    esc_html('<div class="endpoint">');
+                    esc_html('<input type="text" name="wp-mailchimp-gw[endpoints][' . $key . '][slug]" value="' . $endpoint['slug'] . '" readonly/>');
+                    esc_html('<input type="hidden" name="wp-mailchimp-gw[endpoints][' . $key . '][listid]" value="' . $endpoint['listid'] . '" readonly/>');
+                    esc_html('<span id="remove" class="dashicons dashicons-trash"></span></div>');
                 }
             }
             ?>
@@ -61,5 +63,6 @@
         <?php submit_button(__('Save all changes', $this->plugin_name), 'primary', 'submit', true); ?>
 
     </form>
+
 
 </div>
