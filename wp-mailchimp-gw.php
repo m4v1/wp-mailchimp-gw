83105b7631236814a454f1ed9d43d42a3f9cd726
m4v1 <marco.vivi@gmail.com>
2019-10-24 16:54:11 +0200

    changed plugin version to 1.0.3 in files

--- wp-mailchimp-gw.php
+++ wp-mailchimp-gw.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WP Mailchimp Gateway
  * Plugin URI:        https://m4v1.work/wp-mailchimp-gw
  * Description:       This plugin expose a WordPress API entrypoint to manage Mailchimp forms.
- * Version:           1.0.0
+ * Version:           1.0.3
  * Author:            Marco Vivi
  * Author URI:        https://m4v1.work
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined('WPINC') ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('WP_MAILCHIMP_GW_VERSION', '1.0.0');
+define('WP_MAILCHIMP_GW_VERSION', '1.0.3');
 
 /**
  * The code that runs during plugin activation.
